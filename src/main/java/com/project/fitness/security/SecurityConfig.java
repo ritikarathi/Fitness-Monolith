9de9f0d4d31223bf2f78bcf00565c4ea2bfac100
Ritika <ritikarathi2005@gmail.com>
2026-05-17 16:03:22 +0530

    init commit

--- src/main/java/com/project/fitness/security/SecurityConfig.java
+++ src/main/java/com/project/fitness/security/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig {
                         authorizeRequests
                                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui.html/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated());
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
