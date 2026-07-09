649e7868d562d5e618a516e41fe4609ac0a043ee
Ritika <ritikarathi2005@gmail.com>
2026-07-09 11:03:19 +0530

    created the userDto and AuthResponseDto to prevent sending the raw data and passwords as a response

---
+++ src/main/java/com/project/fitness/dto/AuthResponseDto.java
@@ -0,0 +1,18 @@
+package com.project.fitness.dto;
+
+import com.project.fitness.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDto {
+
+    private String token;
+
+    private UserDto user;
+}
---
+++ src/main/java/com/project/fitness/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.project.fitness.dto;
+
+import com.project.fitness.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private String id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private UserRole role;
+}
